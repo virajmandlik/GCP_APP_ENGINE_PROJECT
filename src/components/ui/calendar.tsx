0c8d80e89c95465bf03c09eb33e90e5be579779d
=viraj <=viraj.mandlik@mitaoe.ac.in>
2025-03-18 20:44:03 +0530

    second commit

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons';
+
 import { DayPicker } from 'react-day-picker';
 
 import { cn } from '@/lib/utils';
@@ -58,8 +58,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
+        // IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
+        // IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
       }}
       {...props}
     />
